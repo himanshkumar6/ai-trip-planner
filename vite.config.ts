c2846f274c5ea0cd29ae4af22fd5be2f61e5795a
himanshkumar6 <githubhimanshu0@gmail.com>
2026-03-09 01:09:41 +0530

    fix production routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/", // ⭐ IMPORTANT
+
   plugins: [react()],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
-  server: {
-    proxy: {
-      "/hf": {
-        target: "https://router.huggingface.co",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) =>
-          path.replace(/^\/hf\/models/, "/hf-inference/models"),
-      },
     },
   },
 });
